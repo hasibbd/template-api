e8796dda78cb8a502d148da14423ced1e2092f3f
Hasib <hasib.09512gmail.com>
2022-05-16 18:22:35 +0600

    dynamic menu

--- resources/views/admin/pages/menu-list/index.blade.php
+++ resources/views/admin/pages/menu-list/index.blade.php
@@ -6,12 +6,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">User List</h1>
+                        <h1 class="m-0">Menu Setting</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="{{'dashboard'}}">Home</a></li>
-                            <li class="breadcrumb-item active">User List v1</li>
+                            <li class="breadcrumb-item active">Menu Setting</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
--- resources/views/admin/partial/menu.blade.php
+++ resources/views/admin/partial/menu.blade.php
@@ -1,102 +1,66 @@
 <nav class="mt-2">
-{{--    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
-             with font-awesome or any other icon font library -->
-        <li class="nav-item">
-            <a href="{{url('dashboard')}}" class="nav-link {{ Request::segment(1) == 'dashboard'  ? 'active' : ''}}">
-                <i class="nav-icon fas fa-tachometer-alt"></i>
-                <p>
-                    Dashboard
-                </p>
-            </a>
-
-        </li>
-        <li class="nav-item">
-            <a href="{{url('user-list')}}" class="nav-link {{ Request::segment(1) == 'user-list'  ? 'active' : ''}}">
-                <i class="nav-icon far fa-circle text-info"></i>
-                <p>User List</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="{{url('menu-list')}}" class="nav-link {{ Request::segment(1) == 'menu-list'  ? 'active' : ''}}">
-                <i class="nav-icon far fa-circle text-info"></i>
-                <p>Menu List</p>
-            </a>
-        </li>
-    </ul>--}}
-
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
-             with font-awesome or any other icon font library -->
-       @foreach($menu->where('parent_id', null) as $m)
-           <?php
-            $c = count($menu->where('parent_id', $m->id));
+        @foreach($menu->where('parent_id',null) as $m)
+            <?php
+            $check = $menu->where('parent_id', $m->id)->count();
+
             ?>
         <li class="nav-item
-       <?php
-        if($c > 0){
-            foreach($menu->where('parent_id', $m->id) as $m2){
-                echo  Request::segment(1) == $m2['href']  ? 'menu-is-opening menu-open' : '';
-            }
-        }
-        ?>
-         ">
-         <a href="{{url($m['href'])}}" class="nav-link
-{{ Request::segment(1) == $m['href']  ? 'active' : ''}}
-         <?php
-         if($c > 0){
-             foreach($menu->where('parent_id', $m->id) as $m2){
-                 echo  Request::segment(1) == $m2['href']  ? 'active' : '';
-             }
-         }
-         ?>
-             ">
-                <i class="nav-icon {{$m['icon']}}"></i>
+         @if($check > 0)
+        @foreach($menu->where('parent_id', $m->id) as $m2)
+        {{ Request::segment(1) == $m2->href  ? 'menu-open' : ''}}
+        @endforeach
+        @endif
+            ">
+            <a href="{{$m->href}}" class="nav-link
+             @if($check > 0)
+              @foreach($menu->where('parent_id', $m->id) as $m2)
+                 {{ Request::segment(1) == $m2->href  ? 'active' : ''}}
+               @endforeach
+            @else
+            {{ Request::segment(1) == $m->href  ? 'active' : ''}}
+            @endif
+
+                ">
+                <i class="nav-icon {{$m->icon}}"></i>
                 <p>
-                   {{$m['text']}}
-                    @if($c > 0)
-                    <i class="fas fa-angle-left right"></i>
+                   {{$m->text}}
+                    @if($check > 0)
+                    <i class="right fas fa-angle-left"></i>
                      @endif
                 </p>
             </a>
-          @if($c > 0)
-                <ul class="nav nav-treeview" style="display: none;">
-                @foreach($menu->where('parent_id', $m->id) as $m2)
-                    <li class="nav-item">
-                        <a href="{{url($m2['href'])}}" class="nav-link {{ Request::segment(1) == $m2['href']  ? 'active' : ''}}">
-                            <i class="nav-icon {{$m2['icon']}}"></i>
-                            <p>
-                                {{$m2['text']}}
-                            </p>
-                        </a>
-                    </li>
+
+            @if($check > 0)
+            <ul class="nav nav-treeview small">
+               @foreach($menu->where('parent_id', $m->id) as $m2)
+                    <?php
+                    $check2 = $menu->where('parent_id', $m2->id)->count();
+                    ?>
+                <li class="nav-item">
+                    @if($check2 > 0)
+                        @foreach($menu->where('parent_id', $m2->id) as $m3)
+                            {{ Request::segment(1) == $m3->href  ? 'menu-open' : ''}}
+                        @endforeach
+                    @endif
+                    <a href="{{$m2->href}}" class="nav-link
+                       @if($check2 > 0)
+                    @foreach($menu->where('parent_id', $m2->id) as $m3)
+                    {{ Request::segment(1) == $m3->href  ? 'active' : ''}}
                     @endforeach
-                </ul>
-            @endif
+                    @else
+                    {{ Request::segment(1) == $m2->href  ? 'active' : ''}}
+                    @endif
+                      ">
+                        <i class="nav-icon {{$m2->icon}}"></i>
+                        <p> {{$m2->text}}</p>
+                    </a>
+                </li>
+                @endforeach
+            </ul>
+                @endif
         </li>
         @endforeach
-        <li class="nav-item">
-            <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-search"></i>
-                <p>
-                    Search
-                    <i class="fas fa-angle-left right"></i>
-                </p>
-            </a>
-            <ul class="nav nav-treeview" style="display: none;">
-                <li class="nav-item">
-                    <a href="pages/search/simple.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Simple Search</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="pages/search/enhanced.html" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Enhanced</p>
-                    </a>
-                </li>
-            </ul>
-        </li>
     </ul>
+
 </nav>
