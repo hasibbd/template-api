5b99148ea8a0ef60e60e2982e964ce9f16b52074
Hasib <hasib.09512gmail.com>
2022-06-20 16:58:29 +0600

    update

--- app/Helpers/MenuActivator.php
+++ app/Helpers/MenuActivator.php
@@ -4,8 +4,18 @@ namespace App\Helpers;
 
 class MenuActivator
 {
-    public static function check($data)
+    public static function check($data, $url, $className)
     {
-      return 5455;
+     foreach ($data as $d){
+       if (str_replace( array( '\'', '/','"', ',' , ';', '<', '>' ), '', $d['href']) == str_replace( array( '\'','/', '"', ',' , ';', '<', '>' ), '', $url)){
+           return $className;
+       }
+     }
+    }
+    public static function active($url, $url2)
+    {
+            if (str_replace( array( '\'', '/','"', ',' , ';', '<', '>' ), '', $url) == str_replace( array( '\'','/', '"', ',' , ';', '<', '>' ), '', $url2)){
+                return 'active';
+            }
     }
 }
--- resources/views/admin/partial/menu.blade.php
+++ resources/views/admin/partial/menu.blade.php
@@ -1,22 +1,23 @@
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
         @foreach($menus as $menu)
-            <li class="nav-item @if($menu['children']) menu-open @endif">
-                <a href="{{$menu['href']}}" class="nav-link">
+            <li class="nav-item  {!! MenuActivator::check($menu['children'], Request::segment(1), 'menu-open') !!}">
+                <a href="{{$menu['href']}}" class="nav-link  {!! MenuActivator::check($menu['children'],Request::segment(1), 'active') !!} {!! MenuActivator::active($menu['href'], Request::segment(1)) !!}">
                     <i class="nav-icon  {{$menu['icon']}}"></i>
                     <p>
-                        {{$menu['text']}} {!! MenuActivator::check($menu['children']) !!}
+                        {{$menu['text']}}
                         @if($menu['children'])
-                        <i class="right fas fa-angle-left"></i>
+                            <i class="right fas fa-angle-left"></i>
                         @endif
                     </p>
                 </a>
                 @if($menu['children'])
                     @foreach($menu['children'] as $menu)
-                    <ul class="nav nav-treeview" style="display: none;">
-                        <li class="nav-item">
-                            <a href="{{$menu['href']}}" class="nav-link">
-                                <i class="{{$menu['icon']}} nav-icon"></i>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item{!! MenuActivator::check($menu['children'], Request::segment(1), 'menu-open') !!}">
+                            <a href="{{$menu['href']}}" class="nav-link  {!! MenuActivator::check($menu['children'],Request::segment(1), 'active') !!} {!! MenuActivator::active($menu['href'], Request::segment(1)) !!}">
+                                <i class="nav-icon  {{$menu['icon']}}"></i>
                                 <p>
                                     {{$menu['text']}}
                                     @if($menu['children'])
@@ -26,90 +27,27 @@
                             </a>
                             @if($menu['children'])
                                 @foreach($menu['children'] as $menu)
-                                    <ul class="nav nav-treeview" style="display: none;">
-                                        <li class="nav-item">
-                                            <a href="{{$menu['href']}}" class="nav-link">
-                                                <i class="{{$menu['icon']}} nav-icon"></i>
-                                                <p>
-                                                    {{$menu['text']}}
-                                                    @if($menu['children'])
-                                                        <i class="right fas fa-angle-left"></i>
-                                                    @endif
-                                                </p>
-                                            </a>
-
+                                    <ul class="nav nav-treeview">
+                                        <li class="nav-item{!! MenuActivator::check($menu['children'], Request::segment(1), 'menu-open') !!}">
+                                            <a href="{{$menu['href']}}" class="nav-link  {!! MenuActivator::check($menu['children'],Request::segment(1), 'active') !!} {!! MenuActivator::active($menu['href'], Request::segment(1)) !!}">
+                                                <i class="nav-icon  {{$menu['icon']}}"></i>
+                                            <p>
+                                                {{$menu['text']}}
+                                                @if($menu['children'])
+                                                    <i class="right fas fa-angle-left"></i>
+                                                @endif
+                                            </p>
+                                        </a>
                                         </li>
                                     </ul>
-                                @endforeach
-                            @endif
-                        </li>
-                    </ul>
+                                    @endforeach
+                                    @endif
+                            </li>
+                        </ul>
                     @endforeach
                 @endif
             </li>
         @endforeach
-
-       {{-- @foreach($menu->where('parent_id',null) as $m)
-            <?php
-            $check = $menu->where('parent_id', $m->id)->count();
-
-            ?>
-        <li class="nav-item
-         @if($check > 0)
-        @foreach($menu->where('parent_id', $m->id) as $m2)
-        {{ Request::segment(1) == $m2->href  ? 'menu-open' : ''}}
-        @endforeach
-        @endif
-            ">
-            <a href="{{$m->href}}" class="nav-link
-             @if($check > 0)
-              @foreach($menu->where('parent_id', $m->id) as $m2)
-                 {{ Request::segment(1) == $m2->href  ? 'active' : ''}}
-               @endforeach
-            @else
-            {{ Request::segment(1) == $m->href  ? 'active' : ''}}
-            @endif
-
-                ">
-                <i class="nav-icon {{$m->icon}}"></i>
-                <p>
-                   {{$m->text}}
-                    @if($check > 0)
-                    <i class="right fas fa-angle-left"></i>
-                     @endif
-                </p>
-            </a>
-
-            @if($check > 0)
-            <ul class="nav nav-treeview small">
-               @foreach($menu->where('parent_id', $m->id) as $m2)
-                    <?php
-                    $check2 = $menu->where('parent_id', $m2->id)->count();
-                    ?>
-                <li class="nav-item">
-                    @if($check2 > 0)
-                        @foreach($menu->where('parent_id', $m2->id) as $m3)
-                            {{ Request::segment(1) == $m3->href  ? 'menu-open' : ''}}
-                        @endforeach
-                    @endif
-                    <a href="{{$m2->href}}" class="nav-link
-                       @if($check2 > 0)
-                    @foreach($menu->where('parent_id', $m2->id) as $m3)
-                    {{ Request::segment(1) == $m3->href  ? 'active' : ''}}
-                    @endforeach
-                    @else
-                    {{ Request::segment(1) == $m2->href  ? 'active' : ''}}
-                    @endif
-                      ">
-                        <i class="nav-icon {{$m2->icon}}"></i>
-                        <p> {{$m2->text}}</p>
-                    </a>
-                </li>
-                @endforeach
-            </ul>
-                @endif
-        </li>
-        @endforeach--}}
     </ul>
 
 </nav>
